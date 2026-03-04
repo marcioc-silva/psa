a2b8fe74d5edd9209daabd3cc31ae99bd1ed18c6
marcioc-silva <marcioc.dasilva84@gmail.com>
2026-03-04 14:36:18 -0300

    Delete app directory

--- app/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-login_manager.login_view = "main.login"   # ajuste se seu blueprint/endpoint for outro
-login_manager.login_message_category = "warning"
