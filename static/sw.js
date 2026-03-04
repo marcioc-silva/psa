14a36db8f57527cedece2e3ef1b0d9837246481a
marcioc-silva <marcioc.dasilva84@gmail.com>
2026-03-04 14:37:06 -0300

    Delete static directory

--- static/sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('fetch', function(event) {
-    // Este código básico permite que o navegador reconheça o app como instalável
-    event.respondWith(fetch(event.request));
-});
