f88ab952015931d9ad5bd1adf3e9bec2f286f034
Marcio Correa <marcioc.dasilva84@gmail.com>
2026-03-04 22:01:08 -0300

    Correção mydot

--- app/__init__.py
+++ app/__init__.py
@@ -111,7 +111,7 @@ def create_app(config_object=None):
     app.register_blueprint(dashboard_bp)
     from app.routes.manual import bp as manual_bp
     app.register_blueprint(manual_bp)
-    from mydot.mydot_module.routes.mydot import bp as mydot_bp
+    
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -124,9 +124,10 @@ def create_app(config_object=None):
         try:
             from mydot.mydot_module.routes.mydot import bp as mydot_bp
             app.register_blueprint(mydot_bp, url_prefix="/mydot")
+
+            from mydot.mydot_module.models.ponto import MyDotPunch
             with app.app_context():
                 db.create_all(bind_key="mydot")
-                
         except Exception as e:
             app.logger.warning(f"MyDot desabilitado (não impacta PSA): {e}")
             
